d168b084ac8e925f3cecde16445bfdbc46004c7e
admin <gkaeogus7@gmail.com>
2023-10-17 15:52:30 +0900

    Initial commit

---
+++ src/main/java/com/chocoham/mbtisiteproject/MbtiSiteProjectApplication.java
@@ -0,0 +1,13 @@
+package com.chocoham.mbtisiteproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbtiSiteProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MbtiSiteProjectApplication.class, args);
+    }
+
+}
